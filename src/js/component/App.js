285f0cd87af2c6f9fcf573abc8bda4633860dbe9
Matheus Monteiro <matheusferretti96@gmail.com>
2020-09-12 00:36:56 +0000

    ToDoListJSX

---
+++ src/js/component/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<div className="app">
+			<h1>ToDos</h1>
+			<div className="app-content" />
+		</div>
+	);
+};
+
+export default App;
